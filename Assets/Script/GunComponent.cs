d31c09e8a5ea394f725452c4edcc0d32fc718f5c
sonmarttt <143427384+sonmarttt@users.noreply.github.com>
2026-02-06 09:29:03 -0500

    lab3

--- Assets/Script/GunComponent.cs
+++ Assets/Script/GunComponent.cs
@@ -9,5 +9,36 @@ public class GunComponent : MonoBehaviour
     private float chargeTime = 0.0f;
     private bool isCharging = false;
 
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            chargeTime = 0.0f;
+            isCharging = true;
+        }
+        if(Input.GetButton("Fire1"))
+        {
+            chargeTime += Time.deltaTime;
+            chargeTime = Mathf.Clamp(chargeTime, 0.0f, maxChargeTime);
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            ShootBullet();
+            isCharging = false;
+        } 
+        
+        
+    }
+
+    void ShootBullet( ){
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+
+        float bulletImpulse = (chargeTime / maxChargeTime) * bulletMaxImpulse;
+
+        rb.AddForce(bulletSpawnPoint.forward * bulletImpulse, ForceMode.Impulse);
+    }
+
    
+
 }
